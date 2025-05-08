fccb7bd444f43147364a6fca4b2f5aef66300d9d
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-08 14:37:10 +0530

    push temporal litrals

--- String/script.js
+++ String/script.js
@@ -5,8 +5,9 @@
     const add_data = message + ' ' + intro
     
     const lastFourDigit = '1234'
-    console.log((lastFourDigit.padEnd(6, '*')));
-    console.log((lastFourDigit.padStart(6, '*')));
+    // console.log((lastFourDigit.padEnd(6, '*')));
+    // console.log((lastFourDigit.padStart(6, '*')));
     
     
-    
+    const temp = `hello I added the two lines ${message} ${intro}`
+    console.log(temp);
