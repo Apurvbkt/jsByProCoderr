51ec5ebe5eab9c55c24817379caf7b932d2de405
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-08 10:34:14 +0530

    padend and padstart

--- String/script.js
+++ String/script.js
@@ -3,3 +3,10 @@
     const intro = 'hii there, My name is Apurva'
     
     const add_data = message + ' ' + intro
+    
+    const lastFourDigit = '1234'
+    console.log((lastFourDigit.padEnd(6, '*')));
+    console.log((lastFourDigit.padStart(6, '*')));
+    
+    
+    
