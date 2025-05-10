eb546566cfd22f825f23087ff44b249b904d5bfd
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-10 10:13:03 +0530

    math random

--- mathObject/script.js
+++ mathObject/script.js
@@ -3,8 +3,9 @@ let x = 13/2
 let y = 13%2
 // console.log(y);
 
-console.log(Math.floor(13.8))
-console.log(Math.ceil(13.8));
-console.log(Math.round(13.8));
-console.log(Math.round(13.4));
+// console.log(Math.floor(13.8))
+// console.log(Math.ceil(13.8));
+// console.log(Math.round(13.8));
+// console.log(Math.round(13.4));
+console.log(Math.round( Math.random() * 10));
   
