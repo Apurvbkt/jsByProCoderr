b7f2695adb9d5187cb1f7779a41b223ccb19be5b
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-10 10:35:13 +0530

    push custom random mth

--- mathObject/script.js
+++ mathObject/script.js
@@ -3,9 +3,11 @@ let x = 13/2
 let y = 13%2
 // console.log(y);
 
+let max = 10
+
 // console.log(Math.floor(13.8))
 // console.log(Math.ceil(13.8));
 // console.log(Math.round(13.8));
 // console.log(Math.round(13.4));
-console.log(Math.round( Math.random() * 10));
+console.log(Math.floor( Math.random() * 11) + 20);
   
