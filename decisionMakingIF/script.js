678a084ec0b0a892f8df26dfe47a270b95af8791
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-13 09:59:59 +0530

    age detection

--- decisionMakingIF/script.js
+++ decisionMakingIF/script.js
@@ -1,9 +1,16 @@
-let age =  17;
+let age =  45;
     if (age >= 18) {
         console.log('You are eligible to vote');
         // document.write('You are eligible to vote');
         
     }else{
         console.log('You are not eligible to vote');
-        // document.write('You are not eligible to vote');
+        document.write('You are not eligible to vote');
     }
+    
+    let age2 = 46
+    if(age2>=25 && age2 <=45){
+        console.log("working porfessional");
+        
+    }else console.log("not a working professional");
+    
