3320bc6a96c952749b60da233774f466c12f2741
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-15 11:10:23 +0530

    got it

--- switchCase/ternary.js
+++ switchCase/ternary.js
@@ -15,3 +15,18 @@ let ans = ((name == 'apurv') && (name.length == 5)) ? 'true name matched' : 'fal
 console.log(ans);
 
 
+//2
+
+let gender  = 'f'
+
+let res = `${(gender.toLocaleUpperCase() == 'F') || (gender.toLocaleUpperCase() == 'M') ? 'She' : 'He'  } is a collage student  `
+
+console.log(res);
+
+
+
+let res = (gender.toLocaleUpperCase() === 'F')  ? 'She is a college student' : (gender.toLocaleUpperCase() === 'M') ? 'He is a college student' 
+    : 'Not a student.';
+
+
+
