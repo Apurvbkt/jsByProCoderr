a972c902438f50076bb1c9793d00313dc74fba78
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-15 10:44:08 +0530

    ternary

---
+++ switchCase/ternary.js
@@ -0,0 +1,17 @@
+// true ? 'apurv' : 100
+
+let name = 'apurv'
+
+// if(name == 'apurv'){
+//     console.log('true name matched');
+    
+// }else{
+//     console.log('name not matched');
+    
+// }
+
+ 
+let ans = ((name == 'apurv') && (name.length == 5)) ? 'true name matched' : 'false not matched'
+console.log(ans);
+
+
