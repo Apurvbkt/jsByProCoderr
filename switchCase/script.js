47abe499e573e9c6fa82d8877361c6cb8e0276d0
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-15 09:37:39 +0530

    code written and tested

--- switchCase/script.js
+++ switchCase/script.js
@@ -1,20 +1,54 @@
 
-const dayNumber = 3
 
-if (dayNumber === 0) {
-  console.log('It is Sunday Today')
-} else if (dayNumber === 1) {
-  console.log('It is Monday Today')
-} else if (dayNumber === 2) {
-  console.log('It is Tuesday Today')
-} else if (dayNumber === 3) {
-  console.log('It is Wednesday Today')
-} else if (dayNumber === 4) {
-  console.log('It is Thursday Today')
-} else if (dayNumber === 5) {
-  console.log('It is Friday Today')
-} else if (dayNumber === 6) {
-  console.log('It is Saturday Today')
-} else {
-  console.log('Please Enter a Valid Day Number')
+const dayNumber = 0
+
+debugger
+
+// if (dayNumber === 0) {
+//   console.log('It is Sunday Today')
+// } else if (dayNumber === 1) {
+//   console.log('It is Monday Today')
+// } else if (dayNumber === 2) {
+//   console.log('It is Tuesday Today')
+// } else if (dayNumber === 3) {
+//   console.log('It is Wednesday Today')
+// } else if (dayNumber === 4) {
+//   console.log('It is Thursday Today')
+// } else if (dayNumber === 5) {
+//   console.log('It is Friday Today')
+// } else if (dayNumber === 6) {
+//   console.log('It is Saturday Today')
+// } else {
+//   console.log('Please Enter a Valid Day Number')
+// }
+
+
+// switch case started
+
+switch (dayNumber) {
+    
+    case 0: console.log('it is sunday today');
+    break;
+    
+    case 1: console.log('it is a monday');
+    break;
+    
+    case 2: console.log('it is a tuesday');
+    break;
+    
+    case 3: console.log('it is a wednesday');
+    break;
+    
+    case 4: console.log('it is a thursday');
+    break;
+    
+    case 5: console.log('it is a friday');
+    break;
+    
+    case 6: console.log('it is a saturday');
+    break;
+    
+    
+    
 }
+console.log('program ended');
