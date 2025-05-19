9618d015c48d5d45eb34be4cedbc2c7ff90a65b1
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-19 09:11:21 +0530

    freez and seal done

--- objectFreez&Seal/script.js
+++ objectFreez&Seal/script.js
@@ -4,5 +4,5 @@ const person = {
     age: 23
 }
 
-// Object.freeze(person)
-Object.seal(person)
+// Object.freeze(person)   ===> we cant change anything inside the object, not even update anything inside the object
+// Object.seal(person) ==> we can update the inner content of the of the object, but can add any thing form the ourtside .
