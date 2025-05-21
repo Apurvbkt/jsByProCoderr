ab16cfcf904e7a03f0661aaf2c820fe123036560
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-21 11:18:44 +0530

    multi array

--- arrays/script.js
+++ arrays/script.js
@@ -1,5 +1,12 @@
 let one = [1,2,3,4,6,7]
 let two = [11,12,13,14,15,16,17]
 
-console.log(...one,...two);
+// console.log(...one,...two);
+// console.log(one.reverse());
 
+
+let multi = [['apurv', 23, ['class', 6], ['class', 7] ],
+
+            
+                
+            ['akash', 24, ['class', 8], ['class', 9]]]
