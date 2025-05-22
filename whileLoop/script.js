1ae1e18dd9624051eee5a2f565faaaf82b3f57cd
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-22 11:02:26 +0530

    done while loop

--- whileLoop/script.js
+++ whileLoop/script.js
@@ -1,6 +1,17 @@
 let num = 1;
 
-while(num <=10){
-    console.log('hii', num);
-    num++
+// debugger
+
+// while(num <=10){
+//     console.log('hii', num);
+//     num+= 1
+// }
+
+let dost = ['Adarsh', 'Akash', 'Tri', 'Mukti', 'Papay']
+let i = 0;
+while(i < dost.length){
+    dost[i] = dost[i] + ' Singh';
+    console.log(dost[i]);
+    i++
+
 }
