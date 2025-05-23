d5536711358a0acc8d7dcae6071a6a423221e1e1
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-23 20:24:01 +0530

    even number

--- forLoop/script.js
+++ forLoop/script.js
@@ -4,3 +4,15 @@ for(i = 0; i < dost.length; i++){
     console.log(dost[i]);
     
 }
+
+//even number
+
+for(i=1; i<=100; i++){
+    
+    if(i % 2 == 0){
+        console.log(i);
+        
+    }
+    
+    
+}
