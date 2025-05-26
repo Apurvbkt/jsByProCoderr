a2f32dfafeed93a04d4bb3e039f6fadc4ac6c50e
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-26 20:13:08 +0530

    para 3 done

--- functions/script.js
+++ functions/script.js
@@ -5,14 +5,17 @@ function data(){
 
 // data()
 
-function idCard(x, y){
+function idCard(x, y, z){
     console.log('hii', x);
     console.log(`i am a ${y}`);
-    
+    console.log(`I am ${z} years old.`);
+        
     // aaj mera tbyt kharb hai  to esliye kuch pdhai ni ho paya and sath me ek kaan se sunai ni dera, but hoping sb thik ho jaye mera kaan jldi se, baaki bye kal se padunga
 }
 
 let apu = 'apurv'
+let job = 'web developer'
+let age =  24
 
-idCard('apurv', 'web ')
+idCard('apurv', job, age)
 
