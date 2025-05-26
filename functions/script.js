f08ffc125fba67af810b8dffade8e8e38436b1f3
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-05-26 20:10:38 +0530

    bye

--- functions/script.js
+++ functions/script.js
@@ -9,7 +9,7 @@ function idCard(x, y){
     console.log('hii', x);
     console.log(`i am a ${y}`);
     
-    // aaj mera tbyt kharb hai  to esliye kuch pdhai ni ho paya and sath me ek kaan se sunai ni dera, but hoping sb thik ho jaye mera kaan jldi se
+    // aaj mera tbyt kharb hai  to esliye kuch pdhai ni ho paya and sath me ek kaan se sunai ni dera, but hoping sb thik ho jaye mera kaan jldi se, baaki bye kal se padunga
 }
 
 let apu = 'apurv'
