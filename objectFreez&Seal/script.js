b158ce171208864fabe644cd7e12e36986f37225
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-07 21:37:05 +0530

    push 3

--- objectFreez&Seal/script.js
+++ objectFreez&Seal/script.js
@@ -7,4 +7,4 @@ const person = {
 // Object.freeze(person)   ===> we cant change anything inside the object, not even update anything inside the object
 // Object.seal(person) ==> we can update the inner content of the of the object, but can add any thing form the ourtside .
 
-//ab 
+//ab comback monday hoga
