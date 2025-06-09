155fbaa8141b6ba8241090c1c6b00e9a803f6587
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-09 10:20:11 +0530

    push update

--- objectFreez&Seal/script.js
+++ objectFreez&Seal/script.js
@@ -7,6 +7,3 @@ const person = {
 // Object.freeze(person)   ===> we cant change anything inside the object, not even update anything inside the object
 // Object.seal(person) ==> we can update the inner content of the of the object, but can add any thing form the ourtside .
 
-//ab comback monday hoga, kyuki smj ni aara kuch kal sunday baaki dekhte hai, bye gn
-
-// kal hai come back ka din, kal se padhai suru, pata ni kyu mai mom ko dosh de rha hun, jab mai khud kuch kr ni para, mai frustrated hogay hun. man krta hun sucide krlu. bye good night
