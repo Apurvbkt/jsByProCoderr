cc009a9e7c6d2af02c0f18be6bc6f4b73ef8c25f
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-13 12:20:25 +0530

    psuh timer

--- HOF/setTime.js
+++ HOF/setTime.js
@@ -1,3 +1,10 @@
-setTimeout('console.log("heelo")', 5000) 
+// setTimeout('console.log("heelo")', 5000) 
 
 // setInterval("console.log('hii')", 2000)
+
+let timer0 = setTimeout('console.log("timer0")', 1000)
+let timer1 = setTimeout('console.log("timer1")', 2000)
+let timer2 = setTimeout('console.log("timer2")', 3000)
+let timer3 = setTimeout('console.log("timer3")', 8000)
+
+clearInterval(timer2)
