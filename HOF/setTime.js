63988917a32bdf52cff8e61d045f2cc3a2f1fbac
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-13 21:43:23 +0530

    psuh

--- HOF/setTime.js
+++ HOF/setTime.js
@@ -2,9 +2,17 @@
 
 // setInterval("console.log('hii')", 2000)
 
-let timer0 = setTimeout('console.log("timer0")', 1000)
-let timer1 = setTimeout('console.log("timer1")', 2000)
-let timer2 = setTimeout('console.log("timer2")', 3000)
-let timer3 = setTimeout('console.log("timer3")', 8000)
+// let timer0 = setTimeout('console.log("timer0")', 1000)
+// let timer1 = setTimeout('console.log("timer1")', 2000)
+// let timer2 = setTimeout('console.log("timer2")', 3000)
+// let timer3 = setTimeout('console.log("timer3")', 8000)
 
-clearInterval(timer2)
+// clearInterval(timer2)
+
+
+function a(){
+    console.log('hello world!');
+    
+}
+
+let funTimer = setTimeout(a, 5000)
