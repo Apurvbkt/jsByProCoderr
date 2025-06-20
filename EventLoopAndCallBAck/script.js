bea358e81535123195b316c024a64a9fb8778c98
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-20 11:24:14 +0530

    ḷets start

--- EventLoopAndCallBAck/script.js
+++ EventLoopAndCallBAck/script.js
@@ -1,8 +1,11 @@
-console.log('hii-1');
+// console.log('hii-1');
 
-for(i = 0; i<=4; i++){
-    console.log(i);
+// for(i = 0; i<=4; i++){
+//     console.log(i);
     
-}
+// }
 
-console.log('hi-2');
+// console.log('hi-2');
+
+
+// method and callback
