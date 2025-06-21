8de77271e8cb828f0257c2f74f518b35e9bf9ea1
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-21 21:29:52 +0530

    for each done

--- forEachLoop/script.js
+++ forEachLoop/script.js
@@ -1,2 +1,14 @@
 const frt = ['mango', 'guava', 'orange', 'Apple', 'grapes']
+let dost = ['Adarsh', 'Akash', 'Tri', 'Mukti', 'Papay']
 
+
+frt.forEach(element => {
+    console.log(element);
+    
+});
+
+
+dost.forEach((e) => {
+    console.log(e);
+    
+})
