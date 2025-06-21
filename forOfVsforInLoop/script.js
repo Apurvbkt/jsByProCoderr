218991137cdcfe258692a027ceda65113a56abc7
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-21 12:23:48 +0530

    name

--- forOfVsforInLoop/script.js
+++ forOfVsforInLoop/script.js
@@ -6,11 +6,19 @@ const frt = ['mango', 'guava', 'orange', 'Apple', 'grapes']
 // }
 
 for(indexNum of frt){  //this is the syntex
-    console.log(indexNum);
+    // console.log(indexNum);
     
 }
 
-for(frt of frt){  // this syntex is acceptable (optional)
-    console.log(frt);
+for(frts of frt){  // this syntex is acceptable (optional)
+    // console.log(frts);
+    
+}
+
+
+const name = 'Apurva bhakat'
+
+for(const names of name){
+    console.log(names);
     
 }
