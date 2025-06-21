1dc0b61207a63b8d7a900e3fbc2dff1c70098e36
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-21 11:57:13 +0530

    for of loop done

--- forOfVsforInLoop/script.js
+++ forOfVsforInLoop/script.js
@@ -1,6 +1,16 @@
 const frt = ['mango', 'guava', 'orange', 'Apple', 'grapes']
 
-for(i=0; i< frt.length; i++){
-    console.log(frt[i]);
+// for(i=0; i< frt.length; i++){
+//     console.log(frt[i]);
+    
+// }
+
+for(indexNum of frt){  //this is the syntex
+    console.log(indexNum);
+    
+}
+
+for(frt of frt){  // this syntex is acceptable (optional)
+    console.log(frt);
     
 }
