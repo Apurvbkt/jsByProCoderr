6a8a9b6adba11f5fbc20bf6bb43fb523d1eb43e2
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-21 11:08:03 +0530

    normal fun

---
+++ arrowFun/script.js
@@ -0,0 +1,16 @@
+// Normal function
+
+// function name(num){
+    
+//     return num * num;
+// }
+// console.log(name(2));
+
+
+// function name(num){
+    
+//    console.log(num*num);
+   
+// }
+
+// name(5)
