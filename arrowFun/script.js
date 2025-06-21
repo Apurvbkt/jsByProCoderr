b3e1d74ad0df088f58ab24e6bb61e75e30b6b3e6
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-21 11:48:30 +0530

    done arrow fuhn

--- arrowFun/script.js
+++ arrowFun/script.js
@@ -14,3 +14,19 @@
 // }
 
 // name(5)
+
+
+const sqr = (num) => num * num
+
+const add = (num1, num2) => num1 + num2
+
+console.log(add(5,4));
+
+console.log(sqr(5));
+
+setTimeout(()=>{
+    console.log('hii');
+    
+},2000)
+
+
