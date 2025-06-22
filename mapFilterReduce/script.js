673c5b00ba2e8c790c0e79d19e4fbd0317eff5fa
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-22 13:22:18 +0530

    filter done

--- mapFilterReduce/script.js
+++ mapFilterReduce/script.js
@@ -31,15 +31,17 @@ const months = [
 const mnt =  months.filter((e, index )=>{
                 // console.log(e);
                 
-                for(i = 0; i < e.length; i++){
-                    // console.log(e[i]);
+                // for(i = 0; i < e.length; i++){
+                //     // console.log(e[i]);
                     
-                    if(e[i]=='m' || e[i] == "M"){
-                        console.log(e);
+                //     if(e[i]=='m' || e[i] == "M"){
+                //         console.log(e);
                         
-                    }
+                //     }
                     
-                }
+                // }
+                
+                return e.toLocaleLowerCase().includes('m')
                 
                 
             })
@@ -51,3 +53,66 @@ const mnt =  months.filter((e, index )=>{
 //                 // }  
             
 //                 return e.length >= 5
+
+
+
+
+
+
+
+
+
+const sdt = [
+    {
+        name:'apu',
+        age:24
+    },
+    {
+        name:'bholu',
+        age:16
+    },
+    {
+        name:'ram',
+        age:10
+    },
+    {
+        name:'lal',
+        age:20
+    },
+    {
+        name:'kalu',
+        age:18
+    },
+    {
+        name:'ralu',
+        age:21
+    },
+    {
+        name:'dholu',
+        age:22
+    }
+]
+
+const student = sdt.filter((e)=>{
+    // console.log(e.age );
+    
+    // if(e.age <= 20){
+    //     console.log(e);
+        
+    // }
+
+    return e.age <= 20
+    
+})
+
+const stdname = student.map((e)=>{
+    // console.log(e.name);
+    return e.name
+}).filter((e)=>{
+    // console.log(e);
+    
+    return e.includes('a')
+})
+
+
+console.log(stdname);
