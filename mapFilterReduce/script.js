e5b3fe3930c94447894926d45c1260a888fb64ee
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-22 12:49:30 +0530

    created a function which fliter m

--- mapFilterReduce/script.js
+++ mapFilterReduce/script.js
@@ -9,10 +9,45 @@ const months = [
     
 // })
 
-const mnt =  months.map((e, index )=>{
-                // console.log(index + 1 ,e);  
-                // return e.toLowerCase()
-                // return e.toUpperCase()
+// const mnt =  months.map((e, index )=>{
+//                 console.log(index + 1 ,e);  
+//                 // return e.toLowerCase()
+//                 return e.toUpperCase() // map array return krta hai. to agar kuch return krna hoto go for map
+//             })
+            
+// console.log(mnt);
+
+
+
+// const mnt1 =  months.forEach((e, index )=>{
+//                 console.log(index + 1 ,e);  
+//                 // return e.toLowerCase()
+//                 return e.toUpperCase() // forEach undefine return krta hai.
+//             })
+            
+// console.log(mnt1);
+
+
+const mnt =  months.filter((e, index )=>{
+                // console.log(e);
+                
+                for(i = 0; i < e.length; i++){
+                    // console.log(e[i]);
+                    
+                    if(e[i]=='m' || e[i] == "M"){
+                        console.log(e);
+                        
+                    }
+                    
+                }
+                
+                
             })
             
-console.log(mnt);
+// console.log(mnt);
+
+// // if(e.length <= 5){
+//                 //     console.log(e);                    
+//                 // }  
+            
+//                 return e.length >= 5
