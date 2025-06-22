27fc64db8e7423c22bd0b3d690071707fc4c6178
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-22 10:43:24 +0530

    tried forEach

---
+++ mapFilterReduce/script.js
@@ -0,0 +1,15 @@
+const months = [
+  "January", "February", "March", "April",
+  "May", "June", "July", "August",
+  "September", "October", "November", "December"
+];
+
+// months.forEach((e)=>{
+//     console.log(e);
+    
+// })
+
+months.map((e)=>{
+    console.log(e);
+    
+})
