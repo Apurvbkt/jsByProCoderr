d0a350a172f263714211f20bb1b3bc42a2dbb59e
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-23 10:21:47 +0530

    done by adding all arugments

---
+++ agrumnt/index.js
@@ -0,0 +1,15 @@
+function add(){
+    // console.log(arguments[0]);
+    // console.log(arguments[2]);
+    // console.log(arguments.length);
+    let ans = 0
+    for(i = 0; i< arguments.length; i++){
+        ans += arguments[i]
+    }
+    // console.log(ans);
+    
+    
+    
+    return ans
+    
+}
