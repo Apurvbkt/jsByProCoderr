9c9b47fd244f49ed28ba77891b51f134567db6d7
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-23 10:23:32 +0530

    push by funciton expression

--- agrumnt/index.js
+++ agrumnt/index.js
@@ -1,13 +1,27 @@
-function add(){
+// function add(){
     // console.log(arguments[0]);
     // console.log(arguments[2]);
     // console.log(arguments.length);
+//     let ans = 0
+//     for(i = 0; i< arguments.length; i++){
+//         ans += arguments[i]
+//     }
+//     // console.log(ans);
+    
+    
+    
+//     return ans
+    
+// }
+
+
+
+const add =  function(){
+    
     let ans = 0
     for(i = 0; i< arguments.length; i++){
         ans += arguments[i]
-    }
-    // console.log(ans);
-    
+    } 
     
     
     return ans
