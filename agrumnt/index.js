7962a5ee4688a719ccdd1d65e8600edc51fe7d7e
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-23 10:57:10 +0530

    sefl crete ans

--- agrumnt/index.js
+++ agrumnt/index.js
@@ -31,14 +31,35 @@
 
 
 
-const add =  (...nums)=>{ // we can not use argument word in ES6 because it's old and have some vurnabilities. 
+// const add =  (...nums)=>{ // we can not use argument word in ES6 because it's old and have some vurnabilities. 
         
-    let ans = 0
-    for(i = 0; i< nums.length; i++){
-        ans += nums[i]
-    } 
+//     let ans = 0
+//     for(i = 0; i< nums.length; i++){
+//         ans += nums[i]
+//     } 
     
     
-    return ans
+//     return ans
     
+// }
+
+
+
+  function add(){
+      
+      // console.log(arguments);
+      const nwArr = Array.from(arguments)
+      // console.log(nwArr);
+      
+      
+    // debugger
+   const arr =  nwArr.reduce((acc, crr)=>{
+        // console.log(acc, crr);
+        return acc + crr
+        
+    },0)
+    
+    console.log(arr)
+     
 }
+add(1,2,3,4,5)
