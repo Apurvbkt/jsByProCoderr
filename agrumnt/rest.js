3d4e3eb90d5ac188bf3bc244ba5f3290ace22876
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-24 10:49:59 +0530

    rest operator with reduce

--- agrumnt/rest.js
+++ agrumnt/rest.js
@@ -1,16 +1,28 @@
 const num = [1,2,3,4,5,6]
 
-function add(...agr){
-    // console.log(agr);
+// function add(...agr){
+//     // console.log(agr);
     
-    let num = 0;
-    for(a=0; a< agr.length; a++){
-        num+=agr[a]
-    }
-    console.log(num);
+//     let num = 0;
+//     for(a=0; a< agr.length; a++){
+//         num+=agr[a]
+//     }
+//     console.log(num);
     
     
      
+// }
+
+// add(...num)
+
+
+function add(...arg){
+    // console.log(arg);
+   const ans = arg.reduce((a, c)=>{
+        return a+c
+    },0)
+    console.log(ans);
+    
 }
 
 add(...num)
