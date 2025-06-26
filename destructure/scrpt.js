145380bd7d0442dea1df9fbe78db912b2b652b14
Apurvbkt <91117738+Apurvbkt@users.noreply.github.com>
2025-06-26 11:40:19 +0530

    file create

---
+++ destructure/scrpt.js
@@ -0,0 +1,13 @@
+const color = ['red', 'blue', 'green', 'white', 'black', 'yellow']
+
+
+
+
+const user = {
+    name: 'apu',
+    age: 23,
+    address:{
+        city:'blr',
+        state: 'jhark'
+    },
+}
